2b626828e88aa5c2da8dc05074c2413833abb3b0
Константин Забелин <zabelin.ks@gmail.com>
2012-01-13 04:08:24 +0400

    Initial Commit

---
+++ KinotafTests/KinotafTests.h
@@ -0,0 +1,13 @@
+//
+//  KinotafTests.h
+//  KinotafTests
+//
+//  Created by Константин Забелин on 13.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KinotafTests : SenTestCase
+
+@end
---
+++ KinotafTests/KinotafTests.m
@@ -0,0 +1,32 @@
+//
+//  KinotafTests.m
+//  KinotafTests
+//
+//  Created by Константин Забелин on 13.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KinotafTests.h"
+
+@implementation KinotafTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KinotafTests");
+}
+
+@end
